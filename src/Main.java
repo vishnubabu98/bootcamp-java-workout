aa63f2d2200e1450646a57e884f63ad4db82e987
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-21 13:21:56 +0530

    add exit

--- src/Main.java
+++ src/Main.java
@@ -7,66 +7,76 @@ public class Main
     public static void main(String[] args) {
 
        int a,b,c,opt;
+       while (true)
+       {
         System.out.println("Enter the operation");
-        System.out.println("1:Largest\n 2: Smallest\n 3:Prime\n 4:Even 5:Reverse");
+        System.out.println("1:Largest\n 2: Smallest\n 3:Prime\n 4:Even \n:Reverse\n 0:Exit");
         opt=sc.nextInt();
+        if(opt==0)
+        {
+            break;
+        }
 
-        switch(opt){
+        switch(opt)
+        {
             case 1:
                 System.out.println("enter the three numbers");
-                a=sc.nextInt();
-                b=sc.nextInt();
-                c=sc.nextInt();
+                a = sc.nextInt();
+                b = sc.nextInt();
+                c = sc.nextInt();
                 //checking
-                int temp=a>b?a:b;
-                int largest=c>temp?c:temp;
-                System.out.println("The largest number is: "+largest);
+                int temp = a > b ? a : b;
+                int largest = c > temp ? c : temp;
+                System.out.println("The largest number is: " + largest);
+                break;
             case 2:
                 System.out.println("enter the three numbers");
-                a=sc.nextInt();
-                b=sc.nextInt();
-                c=sc.nextInt();
+                a = sc.nextInt();
+                b = sc.nextInt();
+                c = sc.nextInt();
                 //checking
-                int tem=a<b?a:b;
-                int smallest=c<tem?c:tem;
-                System.out.println("The largest number is: "+smallest);
+                int tem = a < b ? a : b;
+                int smallest = c < tem ? c : tem;
+                System.out.println("The largest number is: " + smallest);
+                break;
             case 3:
-                int num,count=0;
+                int num, count = 0;
                 System.out.println("enter the number to check prime or not");
-                num =sc.nextInt();
-                for(int i =2 ;i<=num-1;i++)
-                {
-                    if(num%i== 0)
-                    {
+                num = sc.nextInt();
+                for (int i = 2; i <= num - 1; i++) {
+                    if (num % i == 0) {
                         count++;
                     }
                 }
 
-                if(count == 0)
+                if (count == 0)
 
                     System.out.println("prime");
                 else
                     System.out.println("not prime");
+                break;
             case 4:
                 int number1;
                 System.out.println("enter the number");
-                number1 =sc.nextInt();
-                if(number1%2==0)
+                number1 = sc.nextInt();
+                if (number1 % 2 == 0)
                     System.out.println("Number is even");
                 else
                     System.out.println("Number is not even");
+                break;
             case 5:
-                int rev=0,rem=0,number2;
+                int rev = 0, rem = 0, number2;
                 System.out.println("enter the number to  reversed");
-                number2 =sc.nextInt();
-                while(number2>0)
-                {
-                    rem =number2%10;
-                    rev =rev*10+rem;
-                    number2 = number2/10;
+                number2 = sc.nextInt();
+                while (number2 > 0) {
+                    rem = number2 % 10;
+                    rev = rev * 10 + rem;
+                    number2 = number2 / 10;
                 }
 
-                System.out.println("Reverse of the number is "+rev);
+                System.out.println("Reverse of the number is " + rev);
+                break;
+        }
 
         }
 
