8e761159a2039b8ef80da5b7e6782417dc5c1770
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-21 12:37:43 +0530

    add largest

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main
+{
+    static Scanner sc = new Scanner(System.in) ;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       int a,b,c,opt;
+        System.out.println("Enter the operation");
+        System.out.println("1:Largest\n");
+        opt=sc.nextInt();
+
+        switch(opt){
+            case 1:
+                System.out.println("enter the three numbers");
+                a=sc.nextInt();
+                b=sc.nextInt();
+                c=sc.nextInt();
+                //checking
+                int temp=a>b?a:b;
+                int largest=c>temp?c:temp;
+                System.out.println("The largest number is: "+largest);
+        }
+
+
     }
 }
