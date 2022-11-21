adc096a52062c56362b8cbd8bba1ff1c64480970
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-21 12:54:17 +0530

    add prime

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main
 
        int a,b,c,opt;
         System.out.println("Enter the operation");
-        System.out.println("1:Largest\n 2: Smallest");
+        System.out.println("1:Largest\n 2: Smallest\n 3:Prime");
         opt=sc.nextInt();
 
         switch(opt){
@@ -30,6 +30,23 @@ public class Main
                 int tem=a<b?a:b;
                 int smallest=c<tem?c:tem;
                 System.out.println("The largest number is: "+smallest);
+            case 3:
+                int num,count=0;
+                System.out.println("enter the number to check prime or not");
+                num =sc.nextInt();
+                for(int i =2 ;i<=num-1;i++)
+                {
+                    if(num%i== 0)
+                    {
+                        count++;
+                    }
+                }
+
+                if(count == 0)
+
+                    System.out.println("prime");
+                else
+                    System.out.println("not prime");
         }
 
 
